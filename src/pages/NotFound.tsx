2b5ebcd12e2580eeaaf25b0267c542f40073f2a4
SumaiyaMoon <sumaiyaaltafmoon@gmail.com>
2023-10-07 12:26:24 +0500

    previous page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import SMButton from "../components/SMButton";
 import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
   const navigate = useNavigate();
-
+const goback = ()=>{
+  window.history.back();
+}
   return (
     <div className="bg_img_home d-flex align-items-center justify-content-center">
     <div className="container row d-flex align-items-center justify-content-center">
@@ -14,6 +16,7 @@ export default function NotFound() {
             <Typography className="py-2" variant="h4">The page you are trying to visit might not exist.</Typography>
             <div className="d-flex align-items-center justify-content-center gap-2 m-2 ">
                 <SMButton onClick={()=>{navigate('/')}} label="Back to Home" className="text-white rounded"/>
+                <SMButton onClick={()=>{goback()}} label="Go back to previous Page" className="text-white rounded"/>
             </div>
         </div>
     </div>
