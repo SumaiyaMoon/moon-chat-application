1258cb0fb34fd9da1d54531d943b1bd4034f9b8a
SumaiyaMoon <sumaiyaaltafmoon@gmail.com>
2023-10-07 11:02:06 +0500

    not found completed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,22 @@
 import { Box, Typography } from "@mui/material";
+import SMButton from "../components/SMButton";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Box>
-        <Typography>NotFound</Typography>
-      </Box>
-    </>
+    <div className="bg_img_home d-flex align-items-center justify-content-center">
+    <div className="container row d-flex align-items-center justify-content-center">
+       
+        <div className="col-6 text-center text-white">
+            <Typography className="py-2" variant="h2">404! Page Not Found.</Typography>
+            <Typography className="py-2" variant="h4">The page you are trying to visit might not exist.</Typography>
+            <div className="d-flex align-items-center justify-content-center gap-2 m-2 ">
+                <SMButton onClick={()=>{navigate('/')}} label="Back to Home" className="text-white rounded"/>
+            </div>
+        </div>
+    </div>
+        </div>
   );
 }
